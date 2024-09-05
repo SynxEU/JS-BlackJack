6c810844805798b08ad41f2603b8fcd7e6cf2421
Jonas Egetoft Knudsen <jonkn0001@elevcampus.dk>
2024-09-05 13:54:28 +0200

    Idek anymore

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
-var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+const suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 var deck;
 var players;
 var currentPlayers = 0;
